1f932087d0730532f6b471ca52fa322e8a0f2689
asanka-code <asanka.code@gmail.com>
2019-03-09 00:23:12 +0000

    Improved by incorporating various individual functions which are used repetitively into single encapsulating functions.

--- cema.py
+++ cema.py
@@ -9,6 +9,142 @@ print(intValue) #prints the ingeter value
 binaryValue = format(intValue, '08b')
 """
 
+def getHammingWeight(integer):
+    '''
+    This function takes an integer and returns the hamming weight.
+    '''
+    string = format(integer, '08b')    
+    count = 0
+    for i in range(len(string)):
+        if(int(string[i]) == 1):
+            count = count + 1
+    return count
+
+
+def genPlainTexts(plainTextLength, numPlainTexts):
+    '''
+    This function generates a matrix of plaintexts and return as a numpy array.
+    plainTextLength = length of the plaintext in bits
+    numPlainTexts = number of plaintexts to generate
+    '''
+    plainTexts = np.random.randint(256, size=(numPlainTexts, plainTextLength))
+    return plainTexts
+
+
+def genHypoMatrix(plainTexts, keyByteIndex):
+    '''
+    This function generates the hypothesis matrix for the first byte of the key
+    by taking a numpy array which has the first bytes of all the plain texts.
+    
+    plainTextBytes = plain text numpy array
+    keyByteIndex = the index of key byte hypothesized by this hypothetical matrix
+    '''
+    plainTextBytes = plainTexts[:,keyByteIndex:keyByteIndex+1]
+
+    # hypotheis matrix is 256xN long (N is the number of plain texts)
+    H = np.zeros(shape=(256, len(plainTextBytes)), dtype=int)
+    #print(H)
+    
+    for i in range(len(plainTextBytes)):
+        #print(format(plainTextBytes[i][0], '08b'))
+        plainByte = plainTextBytes[i][0]
+        for j in range(256):       
+            #print(format(j, '08b'))
+            keyByte = j
+            # XOR operation between the plain text byte and key byte
+            result = plainByte ^ keyByte
+            #print(format(result, '08b'))
+            #print(getHammingWeight(result))
+            H[j,i] = getHammingWeight(result)
+    return H
+
+def getHypoMatrices(plainTexts, keyLengthInBytes):
+    '''
+    This function generates the hypothesis matrix of each key byte and return them
+    as a tuple. Each element of the tuple is a hypothetical matrix.
+    
+    plainTextBytes = plain text numpy array
+    keyLengthInBytes = number of bytes in the key
+    '''
+    H_all = ()
+    
+    for i in range(keyLengthInBytes):
+        H = genHypoMatrix(plainTexts, i)
+        #print(H)
+        H_all = H_all + (H,)
+
+    return H_all
+
+
+def genEMTraces(realKey, plainTexts, traceDuration):
+    '''
+    This function takes the real key, plaintext matrix and trace duration as input
+    in order to generate EM trace matrix for testing purposes.
+    '''
+    # trace matrix size is (traceDuration X number_of_plain_texts)
+    #T = np.zeros(shape=(traceDuration, len(plainTexts)), dtype=int)
+    #T = np.ones(shape=(traceDuration, len(plainTexts)), dtype=int)
+    #T = np.random.randint(2, size=(traceDuration, len(plainTexts)))
+    T = np.random.randint(10, size=(traceDuration, len(plainTexts)))
+
+    for i in range(len(plainTexts)):
+        for j in range(len(realKey)):
+            result = plainTexts[i][j] ^ realKey[j]
+            hw = getHammingWeight(result)
+            #T[j,i] = hw
+            T[j,i] = T[j,i] + hw
+            
+    return T
+
+
+def getCorrelationCoefficient(variable1, variable2):
+    '''
+    This function returns the correlation coefficient of two python lists
+    '''
+    correlation = np.corrcoef(variable1, variable2)[1,0]
+    return correlation
+
+
+def buildCorrelationMatrix(hypoMatrix, traceMatrix):
+    '''
+    This function takes hypothetical matrix and tracematrix to produce correlation
+    matrix.
+    '''
+    # creating correlation matrix (for the first key byte for time being)
+    C = np.zeros(shape=(len(hypoMatrix), len(traceMatrix)), dtype=float)
+    #print(C.shape)
+    
+    for i in range(len(hypoMatrix)):
+        for j in range(len(traceMatrix)):
+            cor = getCorrelationCoefficient(hypoMatrix[i].tolist(), traceMatrix[j].tolist())
+            C[i, j] = cor
+    
+    #print(np.unravel_index(C.argmax(), C.shape))
+    return C
+
+
+def recoverKey(hypoMatrixAll, traceMatrix, keyLengthInBytes):
+    '''
+    This function takes all the hypothesis matrices and EM trace matrices, build
+    corelation matrices for each key byte in order to recover the key.
+    hypoMatrixAll = a tuple variable where each tuple element is a hypothetical matrix
+    traceMatrix = EM trace matrix
+    keyLengthInBytes = number of bytes in the actual key
+    '''
+    key = np.empty([0,0])
+    
+    for i in range(keyLengthInBytes):
+        C = buildCorrelationMatrix(hypoMatrixAll[i], traceMatrix)
+        (keyByte, timeIndex) = np.unravel_index(C.argmax(), C.shape)
+        key = np.append(key, keyByte)
+        
+    return key
+
+###############################################################################
+#           Depreciated functions                                             #
+###############################################################################
+
+"""
 def getHexChar(bitsList):
     '''
     This function takes a list containing 4 bits and return the corresponding
@@ -88,97 +224,4 @@ def getHexFromBitList(bitsList):
         hexString = hexString + " "
         
     return hexString
-
-def getHammingWeight(integer):
-    '''
-    This function takes an integer and returns the hamming weight.
-    '''
-    string = format(integer, '08b')    
-    count = 0
-    for i in range(len(string)):
-        if(int(string[i]) == 1):
-            count = count + 1
-    return count
-
-
-def genPlainTexts(plainTextLength, numPlainTexts):
-    '''
-    This function generates a matrix of plaintexts and return as a numpy array.
-    plainTextLength = length of the plaintext in bits
-    numPlainTexts = number of plaintexts to generate
-    '''
-    plainTexts = np.random.randint(256, size=(numPlainTexts, plainTextLength))
-    return plainTexts
-
-
-def genHypoMatrix(plainTexts, keyByteIndex):
-    '''
-    This function generates the hypothesis matrix for the first byte of the key
-    by taking a numpy array which has the first bytes of all the plain texts.
-    
-    plainTextBytes = plain text numpy array
-    keyByteIndex = the index of key byte hypothesized by this hypothetical matrix
-    '''
-    plainTextBytes = plainTexts[:,keyByteIndex:keyByteIndex+1]
-
-    # hypotheis matrix is 256xN long (N is the number of plain texts)
-    H = np.zeros(shape=(256, len(plainTextBytes)), dtype=int)
-    #print(H)
-    
-    for i in range(len(plainTextBytes)):
-        #print(format(plainTextBytes[i][0], '08b'))
-        plainByte = plainTextBytes[i][0]
-        for j in range(256):       
-            #print(format(j, '08b'))
-            keyByte = j
-            # XOR operation between the plain text byte and key byte
-            result = plainByte ^ keyByte
-            #print(format(result, '08b'))
-            #print(getHammingWeight(result))
-            H[j,i] = getHammingWeight(result)
-    return H
-
-
-def genEMTraces(realKey, plainTexts, traceDuration):
-    '''
-    This function takes the real key, plaintext matrix and trace duration as input
-    in order to generate EM trace matrix for testing purposes.
-    '''
-    # trace matrix size is (traceDuration X number_of_plain_texts)
-    T = np.zeros(shape=(traceDuration, len(plainTexts)), dtype=int)
-
-    for i in range(len(plainTexts)):
-        for j in range(traceDuration):
-            #print(plainTexts[i])
-            #print(plainTexts[i][j])
-            #print(realKey[j])
-            result = plainTexts[i][j] ^ realKey[j]
-            hw = getHammingWeight(result)
-            T[j,i] = hw
-    return T
-
-
-def getCorrelationCoefficient(variable1, variable2):
-    '''
-    This function returns the correlation coefficient of two python lists
-    '''
-    correlation = np.corrcoef(variable1, variable2)[1,0]
-    return correlation
-
-
-def buildCorrelationMatrix(hypoMatrix, traceMatrix):
-    '''
-    This function takes hypothetical matrix and tracematrix to produce correlation
-    matrix.
-    '''
-    # creating correlation matrix (for the first key byte for time being)
-    C = np.zeros(shape=(len(hypoMatrix), len(traceMatrix)), dtype=float)
-    #print(C.shape)
-    
-    for i in range(len(hypoMatrix)):
-        for j in range(len(traceMatrix)):
-            cor = getCorrelationCoefficient(hypoMatrix[i].tolist(), traceMatrix[j].tolist())
-            C[i, j] = cor
-    
-    #print(np.unravel_index(C.argmax(), C.shape))
-    return C
+"""
